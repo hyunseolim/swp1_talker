57e016ef056b99d268b41f163993a629a35ce830
hyunseolim <2rrtum@gmail.com>
2015-10-12 15:18:47 +0000

    ahahahahahahaha

---
+++ app/models/chat.rb
@@ -0,0 +1,5 @@
+class Chat < ActiveRecord::Base
+    
+      t.string :type
+      t.text :content
+end
---
+++ app/models/talk.rb
@@ -0,0 +1,5 @@
+class Talk < ActiveRecord::Base
+
+      t.text :ask
+      t.text :answer
+end
